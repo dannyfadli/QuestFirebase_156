5398f76e8e090df182a04a68e46db65e17efb4ea
dannyfadli <danyfdly95@gmail.com>
2026-01-09 08:17:59 +0700

    menambah DestinasiHome

---
+++ app/src/main/java/com/example/firebase/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.firebase.view.route
+
+import com.example.firebase.R
+import com.example.firebase.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
