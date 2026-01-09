d7edfb5bd42e484b9f6e2e8f7b9d80f789b3f952
dannyfadli <danyfdly95@gmail.com>
2026-01-09 08:16:45 +0700

    menambah ContaierApp

---
+++ app/src/main/java/com/example/firebase/repositori/container/ContainerApp.kt
@@ -0,0 +1,9 @@
+package com.example.firebase.repositori.container
+
+
+import android.app.Application
+import com.example.firebase.repositori.RepositorySiswa
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
